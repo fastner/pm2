9c02259a3ebb0917ca9f6b7db77dd13a83ec9b54
Sebastian Fastner <mail@sebastianfastner.de>
2013-10-02 15:08:58 +0200

    Add support for unix file sockets

--- constants.js
+++ constants.js
@@ -13,7 +13,7 @@ module.exports = {
   DEFAULT_PID_PATH  : p.join(DEFAULT_FILE_PATH, 'pids'),
   DEFAULT_LOG_PATH  : p.join(DEFAULT_FILE_PATH, 'logs'),
   DUMP_FILE_PATH    : p.join(DEFAULT_FILE_PATH, 'dump.pm2'),
-  DAEMON_BIND_ADDR  : 'localhost:6666',
+  DAEMON_BIND_ADDR  : process.env.PM2_SOCKET_ADDR || 'localhost:6666',
   DEBUG             : false,
   WEB_INTERFACE     : process.env.PM2_WEB_PORT || 9615,
   MODIFY_REQUIRE    : false
